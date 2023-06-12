d7fd46cd89234ee4eb430832132092f0de83ff4e
jcamma93 <130416226+jcamma93@users.noreply.github.com>
2023-06-12 10:00:03 -0400

    Add files via upload

---
+++ Async-Drills.js
@@ -0,0 +1,74 @@
+// function myFunction(message) {
+//     return message
+// }
+
+
+// let myMessage = myFunction('Hello World!');
+// console.log(myMessage)
+// setTimeout(function () {
+//     console.log(myMessage)
+// }, 2000);
+
+
+
+
+
+// let getWords = function () {
+//     console.log("Pizza");
+
+//     setTimeout( function () {
+//         console.log("Meatballs")
+//     }, 3000);
+
+//     setTimeout( function () {
+//         console.log("Pepperoni")
+//     }, 2000);
+
+//     setTimeout( function () {
+//         console.log("Pasta")
+//     }, 1000);
+// }
+
+// getWords();
+
+
+
+
+// let done = function () {
+//     console.log("Job's done!")
+// };
+
+// let countdown = function(num, callback) {
+//     if(num===1) {
+//         callback();
+//     } else {
+//         setTimeout( () => {
+//             console.log(num);
+//             countdown( num - 1, callback)
+//         },1000)
+//     }
+// }
+
+// countdown(10, done);
+
+
+let lunchTime = true;
+
+const orderMeSomeFood = function() {
+    return new Promise(function (resovle, reject) {
+        if(lunchTime) {
+            let myLunch = {
+                lunch: "Nachos",
+                drink: "Cola"
+            }
+            resovle(myLunch)
+        } else {
+            let errMessage = new Error("Not time for lunch");
+            reject(errMessage);
+        }
+    })
+}
+
+orderMeSomeFood()
+.then(response => console.log(response))
+.catch(error => console.log(error))
